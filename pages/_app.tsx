d626f3f6499fa0811238e4937b5ca341c51c53f1
Mateus Gruntowski Mendes <mateusgmendes@hotmail.com>
2022-02-27 00:23:03 -0300

    add base url, prettier config, global style and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+import GlobalStyle from "styles/global";
+import theme from "styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
---
+++ styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 10px;
+  }
+
+  body {
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
